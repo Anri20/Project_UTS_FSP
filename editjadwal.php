aa5258e64d48608106c9adfcbdd7674af31aba16
Anri20 <88647934+Anri20@users.noreply.github.com>
2022-09-19 19:09:09 +0700

    template awal

---
+++ editjadwal.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ubah Jadwal</title>
+    <link rel="stylesheet" href="css/editjadwal.css">
+</head>
+<body>
+    <h2>Ubah Jadwal</h2>
+    <button><a href="index.php">Kembali</a></button>
+</body>
+</html>
